a53ee5b5f23b8bb1c767b856b13d87c9c0e62f0a
mezz39 <ezz54809@gmail.com>
2025-12-09 16:11:08 +0200

    all changes

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -1,15 +1,17 @@
-from .BaseController import BaseController
+from BaseController import BaseController
 from fastapi import FastAPI, APIRouter, Depends, UploadFile
 class DataController(BaseController):
     def __init__(self):
         super().__init__()
         self.size_scale = 1048576
 
-    def validate_uploaded_file(self, file: UploadFile):
+    async def validate_uploaded_file(self, file: UploadFile):
+        contents = await file.read()
+        file_size = len(contents)
         if file.content_type not in self.app_settings.ALLOWED_FILE_TYPES :
             raise ValueError(f"File type {file.content_type} is not allowed.")
 
-        if file.size > self.app_settings.FILE_MAX_SIZE * self.size_scale:
+        if file_size > self.app_settings.FILE_MAX_SIZE * self.size_scale:
             raise ValueError(f"File size exceeds the maximum limit of {self.app_settings.FILE_MAX_SIZE} MB.")
         
         return True
